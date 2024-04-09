867e44997781474a739191c10f94c709afbf4112
Hyun jinyong <ekwls20@gmail.com>
2024-04-09 16:03:38 +0900

    #6 메인페이지 디자인 수정

--- src/components/main/MainExplain.vue
+++ src/components/main/MainExplain.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-row mb-5 mt-14 bg-white pt-10 pb-10">
+  <div class="flex flex-row bg-white pt-16 pb-16">
     <div class="header-under text-4xl font-bold">Welcome to ViewGorithm!</div>
     <div class="vertical-line ml-2 mr-1"></div>
     <div class="header-under font-mono mr-2 ml-1">
