1f75ff36a04444c2bacc6b2f2c6c236107a921a9
Hyun jinyong <ekwls20@gmail.com>
2024-04-17 10:33:31 +0900

    #8 primevue 라이브러리 제거 -Lara 스타일 presets 삭제 -primevue 관련 import 코드 및 base.css 파일 삭제

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 
 import './style.css';
-import './assets/base.css';
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
@@ -8,26 +7,11 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
-// PrimeVue import
-import PrimeVue from 'primevue/config';
-import "primeicons/primeicons.css";
-import Lara from '@/assets/lara';
-// PrimeVue components
-import Button from 'primevue/button';
-// PrimeVue Table
-import DataTable from 'primevue/datatable';
-import Column from 'primevue/column';
-
 const app = createApp(App)
 
 
 app.use(createPinia())
 app.use(router)
-
-// PrimeVue use
-app.use(PrimeVue, { unstyled: true, pt: Lara});
-app.component('DataTable', DataTable);
-app.component('Column', Column);
-app.component('Button', Button);
+    
 
 app.mount('#app')
