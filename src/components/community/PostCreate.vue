4a662772763e9b719238458f395e767c7b208a5c
Hyun jinyong <ekwls20@gmail.com>
2024-05-07 19:45:46 +0900

    #10 게시글 생성 구현중

---
+++ src/components/community/PostCreate.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="modal-overay">
+        <div class="modal-container">
+            <QuillEditor theme="snow" />
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+.modal-overlay {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: rgba(0, 0, 0, 0.5);
+}
+
+.modal-container {
+    position: relative; 
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    width: 90%;
+    height: 90%;
+    background: #fff;
+    border-radius: 10px;
+    padding: 3%;
+    margin-top: 20px;
+    flex-flow: column wrap;
+    align-content: flex-end;
+    overflow: scroll;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -6,7 +6,11 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
-import VueCodeHighlight from 'vue-code-highlight';
+import VueCodeHighlight from 'vue-code-highlight'
+import { QuillEditor } from '@vueup/vue-quill'
+import '@vueup/vue-quill/dist/vue-quill.snow.css';
+
+
 
 const app = createApp(App)
 
@@ -14,6 +18,7 @@ const app = createApp(App)
 app.use(createPinia())
 app.use(router)
 app.use(VueCodeHighlight)
+app.component('QuillEditor', QuillEditor)
 
 
 app.mount('#app')
