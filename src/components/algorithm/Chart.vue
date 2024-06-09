e27ab7df532bf83a9e6cda459d8350e6056ea246
zzdh8 <ekwls20@gmail.com>
2024-06-09 23:43:09 +0900

    #16 props 차트 뷰 적용

--- src/components/algorithm/Chart.vue
+++ src/components/algorithm/Chart.vue
@@ -1,9 +1,10 @@
 <template>
-  <h3 class="text-xl font-oswald">Time Complexity</h3>
-  <p class="pb-6">
+  <h3 class="text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight mb-4">Big-O Complexity</h3>
+  <div class="text-lg my-5">The average time complexity of this algorithm is <span class=" rounded-3xl bg-zinc-900 px-4 py-2 font-bold text-white">{{ timeComplexity }}</span></div>
+  <Line class="chart" :data="data" :options="options" />
+  <p class="py-10 text-xs text-red-500">  
     It basically only shows 'average' and we're going to add the worst and best in the soon.
   </p>
-  <Line class="chart" :data="data" :options="options" />
 </template>
   
   <script>
@@ -26,6 +27,12 @@ export default {
   name: 'App',
   components: {
     Line
+  },
+  props:{
+    timeComplexity: {
+      type: String,
+      required: true
+    }
   },
   data() {
     return chartConfig
