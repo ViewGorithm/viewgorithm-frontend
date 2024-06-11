6d2585178aecd1dcb6ec151181d0f41ba2f95b2c
zzdh8 <ekwls20@gmail.com>
2024-06-12 01:53:10 +0900

    #14 게시글 등록 API 수정 -API JS 파일 수정 -> 불필요하다고 판단한 코드 제거(.then 메소드 제거) -게시글 등록 API 연결에 따른 컴포넌트 수정

--- src/api/axios.post.js
+++ src/api/axios.post.js
@@ -1,29 +1,31 @@
+import { userAuth } from '@/stores/userAuth';
 import axios from 'axios'
+import { ref } from 'vue';
 
 axios.defaults.baseURL = 'http://localhost:8088';
 
-export async function getPosts(){
-    try{
+export async function getPosts() {
+    try {
         const data = await axios.get('/user/post').then(res => res.data);
         return data;
-    }catch(e){
+    } catch (e) {
         console.error(e);
     }
 }
 
-export async function getPost(id){
-    try{
+export async function getPost(id) {
+    try {
         const data = await axios.get(`/user/post/${id}`).then(res => res.data);
         return data;
-    }catch(e){
+    } catch (e) {
         console.error(e);
     }
 }
 
-export async function createPost(post){
-    try{
-        await axios.post('/user/post', post).then(res => console.log(res.status), console.error);
-    }catch(e){
+export async function createPost(post) {
+    try {
+        await axios.post('/user/post/save', post)
+    } catch (e) {
         console.error(e);
     }
 }
--- src/components/community/PostCreate.vue
+++ src/components/community/PostCreate.vue
@@ -10,22 +10,62 @@
           &times;
         </button>
       </div>
-      <QuillEditor theme="snow" toolbar="minimal" />
-      <button class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded m-2">
+      <div class="modal-body">
+        <input type="text" v-model="title" placeholder="Enter title" class="w-full p-2 border border-gray-300 rounded mb-4" />
+      </div>
+      <QuillEditor ref="content" theme="snow" toolBar="minimal"/>
+      <button
+        class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded m-2"
+        @click="create()"
+      >
         Upload
       </button>
+      <div class="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+        <Alert :message="resultText" :isVisible="resultText!=''" />
+      </div>
     </div>
   </div>
 </template>
 
 <script setup>
-const emit = defineEmits(['close-create-modal'])
+import { createPost } from '@/api/axios.post'
+import router from '@/router';
+import { userAuth } from '@/stores/userAuth'
+import { ref } from 'vue';
+import Alert from '@/components/customComponents/Alert.vue'
 
+const emit = defineEmits(['close-create-modal'])
 const closeCreateModal = () => {
   emit('close-create-modal')
 }
 
+const title = ref('')
+const content = ref(null)
 
+const resultText = ref('')
+
+function create() {
+  const auth = userAuth()
+  const post = {
+    title: title.value,
+    content: content.value.getText(),
+    created_at: new Date('yyyy-mm-dd')
+  }
+  if (auth.isLoggedIn) {
+    createPost(post).then((res) => {
+      resultText.value = "Successfully uploaded!"
+      setTimeout(() => {
+        resultText.value = ''
+        router.go(0)
+      }, 1800)
+    })
+  } else {
+    resultText.value = 'You need to login first. Or Please Check to Join Our Service.'
+    setTimeout(()=>{
+      resultText.value = ''
+    }, 1500)
+  }
+}
 </script>
 
 <style lang="scss">
@@ -52,5 +92,4 @@ const closeCreateModal = () => {
     width: 100%;
   }
 }
-
 </style>
