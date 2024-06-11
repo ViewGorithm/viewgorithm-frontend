815bfbce54791838892eefb1ccff3466a5bc0016
zzdh8 <ekwls20@gmail.com>
2024-06-12 02:02:24 +0900

    #18 알림 컴포넌트 구현

---
+++ src/components/customComponents/Alert.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="alert bg-green-500 text-white text-3xl opacity-83 transition-opacity duration-600 p-5 mb-15 rounded-2xl" v-if="isVisible">
+        {{ message }}
+    </div>
+</template>
+
+<script setup>
+import { defineProps } from 'vue'
+
+const props = defineProps({
+    message: {
+        type: String,
+        required: true
+    },
+    isVisible: {
+        type: Boolean,
+        default: true
+    }
+})
+</script>
+
+<style scoped>
+.alert {
+    margin-bottom: 15px;
+}
+</style>
