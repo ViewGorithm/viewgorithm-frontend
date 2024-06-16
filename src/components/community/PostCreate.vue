e4b6c9b6cc8ef26410a85292c93eb474861db11b
Hyun jinyong <ekwls20@gmail.com>
2024-06-16 18:28:06 +0900

    #19 일부 로직 변경 - 기존 게시글 업로드 api를 import 해오지 않고 해당 컴포넌트에서 api 구현 - post 객체 변수 삭제 - api 요청 시 파라미터로 적용되도록 수정 - 시인성을 위한 띄어쓰기 추가

--- src/components/community/PostCreate.vue
+++ src/components/community/PostCreate.vue
@@ -5,31 +5,30 @@
         <h1 class="text-left text-4xl mx-1 my-5 font-mono">Create A Post</h1>
         <button
           class="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:text-white hover:bg-custom-color font-bold w-7 h-7 rounded-xl"
-          @click="closeCreateModal"
-        >
+          @click="closeCreateModal">
           &times;
         </button>
       </div>
-      <div class="modal-body">
-        <input type="text" v-model="title" placeholder="Enter title" class="w-full p-2 border border-gray-300 rounded mb-4" />
+      <div class="modal-body">document
+        <input type="text" v-model="title" placeholder="Enter title"
+          class="w-full p-2 border border-gray-300 rounded mb-4" />
       </div>
-      <QuillEditor ref="content" theme="snow" toolBar="minimal"/>
+      <QuillEditor ref="content" theme="snow" toolBar="minimal" />
       <button
         class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded m-2"
-        @click="create()"
-      >
+        @click="create">
         Upload
       </button>
       <div class="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-        <Alert :message="resultText" :isVisible="resultText!=''" />
+        <Alert :message="resultText" :isVisible="resultText != ''" />
       </div>
     </div>
   </div>
 </template>
 
 <script setup>
-import { createPost } from '@/api/axios.post'
 import router from '@/router';
+import { instance } from '@/api/index-api';
 import { userAuth } from '@/stores/userAuth'
 import { ref } from 'vue';
 import Alert from '@/components/customComponents/Alert.vue'
@@ -46,13 +45,12 @@ const resultText = ref('')
 
 function create() {
   const auth = userAuth()
-  const post = {
-    title: title.value,
-    content: content.value.getText(),
-    created_at: new Date('yyyy-mm-dd')
-  }
   if (auth.isLoggedIn) {
-    createPost(post).then((res) => {
+    instance.post('/user/post/save', {
+      title: title.value,
+      content: content.value.getText(),
+      created_at: new Date()
+    }).then((res) => {
       resultText.value = "Successfully uploaded!"
       setTimeout(() => {
         resultText.value = ''
@@ -61,7 +59,7 @@ function create() {
     })
   } else {
     resultText.value = 'You need to login first. Or Please Check to Join Our Service.'
-    setTimeout(()=>{
+    setTimeout(() => {
       resultText.value = ''
     }, 1500)
   }
@@ -76,6 +74,7 @@ function create() {
   width: 100%;
   height: 100%;
   background-color: rgba(0, 0, 0, 0.5);
+
   .modal-container {
     background-color: white;
     top: 50%;
@@ -83,6 +82,7 @@ function create() {
     padding: 20px;
     border-radius: 5px;
   }
+
   .modal-header {
     display: flex;
     justify-content: space-between;
